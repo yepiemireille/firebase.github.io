4dda3dd1ac1cb7082e1d72029bac8aff30352ffe
yepiemireille <63556518+yepiemireille@users.noreply.github.com>
2020-05-26 03:00:53 +0100

    Add files via upload

---
+++ database.php
@@ -0,0 +1,30 @@
+
+
+<?php 
+   
+   define('DB_HOST', 'localhost');
+   define('DB_NAME', 'miachmo_construction');
+   define('DB_USERNAME', 'root');
+   define('DB_PASSWORD', '');
+   
+   try {
+
+   	    $db = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USERNAME, DB_PASSWORD);
+
+   	    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	   
+
+
+   	
+   } catch (PDOException $e) {
+
+   	     die('Erreur: '.$e->getmessage());
+   	
+   }
+
+   
+
+
+	
+
+
